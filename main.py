04eec41894432f350bb88046470bb4e54ceb7e01
Jimmy <cyncatron@gmail.com>
2021-05-08 20:57:52 -0400

    outline of model

--- main.py
+++ main.py
@@ -36,21 +36,21 @@ def semantics_interface(tokens):
     ############################
     # Grammar rules
     ############################
-      S[SEM  = <?sub(?vp)>]  -> NP[SEM=?subj] VP[SEM=?vp]
-      PP[SEM = <?prep(?np)>] -> P[SEM=?prep] NP[SEM=?np]
-      NP[SEM = <?det(?nom)>] -> DT[SEM=?det] N[SEM=?nom]
-      N[SEM  = <?nom(?pp)>]  -> N[SEM=?nom] PP[SEM=?pp]
-      N[SEM = <?adj(?nom)>]  -> Adj[SEM=?adj] N[SEM=?nom]
-      VP[SEM = <?iv>] -> IV[SEM=?iv]
-      VP[SEM = <?v(?obj)>] -> TV[SEM=?v] NP[SEM=?obj]
-      VP[SEM = <?v(?obj(?obj))>] -> DTV[SEM=?v] NP[SEM=?obj] NP[SEM=?obj]
-      VP[SEM = <?v(?obj(?prep))>] -> DTV[SEM=?obj] NP[SEM=?obj] PP[SEM=?prep]
-      VP[SEM = <?vp(?pp)>] -> VP[SEM=?vp] PP[SEM=?vp]
-      VP[SEM = <?v(?vp)>] -> SV[SEM=?v] S[SEM=?vp]
-      VP[SEM = <?aux(?vp)>] -> AUX[SEM = <?aux>] VP[SEM = <?vp>]
-      VP[SEM = <?v(?vp)>] -> ADV[SEM=?v] VP[SEM=?vp]  
-      NP[SEM=<?pn>] -> PN[SEM=?pn]
-      NP[SEM=<?prn>] -> PRN[SEM=?prn]
+      S -> NP VP
+      PP -> P NP
+      NP -> DT N
+      N -> N PP
+      N -> Adj N
+      VP -> IV
+      VP -> TV NP
+      VP -> DTV NP NP
+      VP -> DTV NP PP
+      VP -> VP PP
+      VP -> SV S
+      VP -> AUX VP
+      VP -> ADV VP  
+      NP -> PN
+      NP -> PRN
     ############################ 
       DT[SEM=<\P Q.exists x.((P(x) -> Q(x)))>] -> 'a'
       DT[SEM=<\P Q.exists x.((P(x) -> Q(x)))>] -> 'the'
@@ -84,69 +84,139 @@ def semantics_interface(tokens):
       C -> that 
 
       WP -> who what 
-      ITV[PFORM=] -> act adapt crawl danse erupt escape leave start party panic
-      TV[sem=<\x x.x(\y.grab(x,y))] -> grab 
-      TV[sem=<\x x.x(\y.impower(x,y))] -> impower 
-      TV[sem=<\x x.x(\y.hold(x,y))] -> hold 
-      TV[sem=<\x x.x(\y.push(x,y))] -> push 
-      TV[sem=<\x x.x(\y.build(x,y))] -> build 
-      TV[sem=<\x x.x(\y.mold(x,y))] -> mold 
-      TV[sem=<\x x.x(\y.hug(x,y))] -> hug 
-      TV[sem=<\x x.x(\y.love(x,y))] -> love 
-      TV[sem=<\x x.x(\y.juice(x,y))] -> juice 
-      TV[sem=<\x x.x(\y.obliterate(x,y))] -> obliterate 
-      N[SEM=<\x.man(x)>]-> man 
-      N[SEM=<\x.man(x)>] ->boy 
-      N[SEM=<\x.pet(x)>] ->cat 
-      N[SEM=<\x.pet(x)>] ->dog 
-      N[SEM=<\x.time(x)>] ->time 
-      N[SEM=<\x.home(x)>] ->house 
-      N[SEM=<\x.company(x)>] ->company 
-      N[SEM=<\x.cow(x)>] ->cow 
-      N[SEM=<\x.program(x)>] ->program 
-      N[SEM=<\x.study(x)>] ->study 
-      N[SEM=<\x.owner(x)>] ->owner 
-      N[SEM=<\x.man(x)>] ->door 
-      N[SEM=<\x.check(x)>] ->check 
-      N[SEM=<\x.corner(x)>] ->corner 
-      N[SEM=<\x.job(x)>] ->job 
-      N[SEM=<\x.dealership(x)>] ->dealership 
-      N[SEM=<\x.office(x)>] ->office 
-      N[SEM=<\x.customer(x)>] ->customer 
-      N[SEM=<\x.sailor(x)>] ->sailor 
-      N[SEM=<\x.man(x)>] ->member 
-      N[SEM=<\x.man(x)>] ->employee
+      ITV -> act adapt crawl danse erupt escape leave start party panic
+      TV[SEM=<\\x x.x(\y.grab(x,y))] -> grab 
+      TV[SEM=<\\x x.x(\y.impower(x,y))] -> impower 
+      TV[SEM=<\\x x.x(\y.hold(x,y))] -> hold 
+      TV[SEM=<\\x x.x(\y.push(x,y))] -> push 
+      TV[SEM=<\\x x.x(\y.build(x,y))] -> build 
+      TV[SEM=<\\x x.x(\y.mold(x,y))] -> mold 
+      TV[SEM=<\\x x.x(\y.hug(x,y))] -> hug 
+      TV[SEM=<\\x x.x(\y.love(x,y))] -> love 
+      TV[SEM=<\\x x.x(\y.juice(x,y))] -> juice 
+      TV[SEM=<\\x x.x(\y.obliterate(x,y))] -> obliterate 
 
-      np[sem=<\p.p(jimmy)>] -> Jimmy
-      np[sem=<\p.p(jimmy)>] -> James 
-      np[sem=<\p.p(jimmy)>] -> Jim
-      np[sem=<\p.p(jordan)>] -> Jordan
-      np[sem=<\p.p(jimmy)>] -> Grant
-      np[sem=<\p.p(jimmy)>] -> Holtzman 
-      np[sem=<\p.p(jimmy)>] -> Bob 
-      np[sem=<\p.p(jimmy)>] -> Joe  
-      np[sem=<\p.p(jimmy)>] -> Jeff 
-      np[sem=<\p.p(jimmy)>] -> George 
+      N[SEM=<\\x.man(x)>]-> man 
+      N[SEM=<\\x.man(x)>] ->boy 
+      N[SEM=<\\x.pet(x)>] ->cat 
+      N[SEM=<\\x.pet(x)>] ->dog 
+      N[SEM=<\\x.time(x)>] ->time 
+      N[SEM=<\\x.home(x)>] ->house 
+      N[SEM=<\\x.company(x)>] ->company 
+      N[SEM=<\\x.cow(x)>] ->cow 
+      N[SEM=<\\x.program(x)>] ->program 
+      N[SEM=<\\x.study(x)>] ->study 
+      N[SEM=<\\x.owner(x)>] ->owner 
+      N[SEM=<\\x.man(x)>] ->door 
+      N[SEM=<\\x.check(x)>] ->check 
+      N[SEM=<\\x.corner(x)>] ->corner 
+      N[SEM=<\\x.job(x)>] ->job 
+      N[SEM=<\\x.dealership(x)>] ->dealership 
+      N[SEM=<\\x.office(x)>] ->office 
+      N[SEM=<\\x.customer(x)>] ->customer 
+      N[SEM=<\\x.sailor(x)>] ->sailor 
+      N[SEM=<\\x.man(x)>] ->member 
+      N[SEM=<\\x.man(x)>] ->employee
+
+      np[SEM=<\P.P(jimmy)>] -> 'jimmy'
+      np[SEM=<\P.P(jimmy)>] -> 'james' 
+      np[SEM=<\P.P(jimmy)>] -> 'jim'
+      np[SEM=<\P.P(jordan)>] ->'jordan'
+      np[SEM=<\P.P(jimmy)>] -> 'grant'
+      np[SEM=<\P.P(sarah)>] -> 'sarah' 
+      np[SEM=<\P.P(bob)>] -> 'bob'
+      np[SEM=<\P.P(dave)>] -> 'dave'  
+      np[SEM=<\P.P(jeff)>] -> 'jeff'
+      np[SEM=<\P.P(george)>] -> 'george' 
 
       VPB ->sneeze
-    """        
+      """
     
-    
-    def create_model(v):
-        value = nltk.Valuation.fromstring(v)    
+    def create_model():
+        v = """
+        jimmy => ji 
+        jordan => jo 
+        jeff => je
+        dave => d 
+        george => g 
+        bob => b
+        sarah => s
+
+
+
+        grab       =>{}  
+        impower    =>{} 
+        hold       =>{}
+        push       =>{}
+        build      =>{}
+        mold       =>{}
+        hug        =>{}
+        love       =>{}
+        juice      =>{}
+        obliterate =>{}
+
+        man       =>{m1}
+        boy       =>{b1}
+        cat       =>{ca1}
+        dog       =>{d1}
+        time      =>{t1}
+        house     =>{h1}
+        company   =>{co1}
+        cow       =>{cw1}
+        program   =>{p1}
+        study     =>{s1}
+        owner     =>{o1}
+        door      =>{do1}
+        check     =>{ch1}
+        corner    =>{c1}
+        job       =>{j1}
+        dealership=>{de1} 
+        office    =>{o1}
+        customer  =>{cu1}
+        sailor    =>{sa1}
+        member    =>{me1}
+        employee  =>{e1}
+
+        
+
+        grab    =>     {}
+        impower    =>  {}
+        hold    =>     {}
+        push    =>     {}
+        build    =>    {}
+        mold    =>     {}
+        hug    =>      {}
+        love    =>     {}
+        juice    =>    {}
+        obliterate   =>{}
+
+        funny => {ji}
+        study   => {jo}
+        tall    => {d}
+        strong  => {}
+        powerful=> {} 
+        short   => {}
+        huge    => {}
+        funny   => {}
+        smart   => {}
+        nice    => {}
+        mean    => {}
+        skinny  => {}
+        big     => {}
+        large  '=> {}
+
+
+        """
+        value = nltk.Valuation.fromstring()    
         init = nltk.Assignment(value.domain)
         m = nltk.Model(value.domain,value)
         return (m,init) 
     
-    def verb_checking(verb,noun1,noun2,m,init):
-       check_string= f"{verb}({noun1},{noun2})" 
-       return m.evaluate(check_string,init) 
+ #   def eval(sen,)
+
+    
+
     
     print(tokenize("what is there?"))
     #determine_type("A man entered the dealership.")
-  """  
-    # part 4
-def CFG():
   
-my_grammar = nltk.CFG.fromstring(string)
-
